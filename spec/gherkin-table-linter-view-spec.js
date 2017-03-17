445b390d665cfd15dc333e718c0d35b683b80313
Victor Dreo <victor.dreo@arkena.com>
2017-03-17 17:49:13 +0100

    Initial working project

---
+++ spec/gherkin-table-linter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GherkinTableLinterView from '../lib/gherkin-table-linter-view';
+
+describe('GherkinTableLinterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
